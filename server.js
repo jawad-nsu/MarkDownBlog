c9f6e3c834aa236e2de5584913106d5042b5bb1f
jawad-nsu <jawadurrahim1997@gmail.com>
2021-04-18 15:14:11 +0600

    Using slugfy to remove ugly ids and more beautiful url extension

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const Artist = require('./models/article')
+const Article = require('./models/article')
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded( {extended: false}))
+
+app.use('/articles', articleRouter)
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort( {createdAt: 'desc'})
+    res.render('articles/index', {articles : articles})
+})
+
+
+
+app.listen(5000);
