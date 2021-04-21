4e13aeb0fddffc647fa6154fdf796baa7d65a316
jawad-nsu <jawadurrahim1997@gmail.com>
2021-04-21 23:49:18 +0600

    Added the edit button

--- routes/articles.js
+++ routes/articles.js
@@ -12,7 +12,7 @@ router.get('/edit/:id', async (req, res) => {
 })
 
 router.get('/:slug', async (req, res) => {
-    const article = await Article.findOne({ slug: req.params.slug })
+    const article = await Article.findOne({ slug: req.params.slug }) //find returns an array of articles, but we only need one, thats we used findone
     if(article == null) res.redirect('/')
     res.render('articles/show', { article: article})
 })
